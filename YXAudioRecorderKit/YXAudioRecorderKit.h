8c162b4ee822a06c41b275039e0d38649ab9f7cf
yuxiang <yuxiang@yuxiangdeMac-mini.local>
2018-06-28 14:53:43 +0800

    framework initialize

---
+++ YXAudioRecorderKit/YXAudioRecorderKit.h
@@ -0,0 +1,19 @@
+//
+//  YXAudioRecorderKit.h
+//  YXAudioRecorderKit
+//
+//  Created by yuxiang on 2018/6/28.
+//  Copyright © 2018年 yuxiang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for YXAudioRecorderKit.
+FOUNDATION_EXPORT double YXAudioRecorderKitVersionNumber;
+
+//! Project version string for YXAudioRecorderKit.
+FOUNDATION_EXPORT const unsigned char YXAudioRecorderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YXAudioRecorderKit/PublicHeader.h>
+
+
